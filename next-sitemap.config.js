0f819a5d96e3cf8bdf036e6c0e34981e0f723e0c
Hanzala-Sarang <Hanzalasarang01@gmail.com>
2025-04-20 19:07:14 +0530

    site-map added

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://nextgrid.solutions', 
+  generateRobotsTxt: true,           // Generates robots.txt file also
+  changefreq: 'daily',
+  priority: 0.7,
+};
